d11f1c8f0c2294bcec188149b80a790107a9d0cb
Superman2971 <goldfarb.ian@gmail.com>
2014-09-19 15:25:56 -0700

    Fixed assets, heroku still not loading JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'homes/index'
+  get 'homes/index', to: 'homes#index'
   get 'homes/timeline', to: 'homes#timeline'
   get 'homes/showreel', to: 'homes#d3ShowReel'
-  get 'cars/index'
-  get 'cars/timeline'
-  get 'cars/showreel'
+  get 'cars/index', to: 'cars#index'
+  get 'cars/timeline', to: 'cars#timeline'
+  get 'cars/showreel', to: 'cars#showreel'
 
   # root path below
   root 'homes#index'
